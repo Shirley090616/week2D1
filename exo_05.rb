680fa8e620de193dd760386aae7cbde5505baaaa
shirley Valeton <shirley.valeton88@gmail.com>
2024-09-30 18:41:47 +0200

    ajouter

---
+++ exo_05.rb
@@ -0,0 +1,18 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "travail : #{10 * 5 * 11 * 60}"
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+puts "ça fait combien 3 + 2 ? #{3 + 2}"
+puts "ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "c'est drole ça, faisons-en plus :"
+
+puts "est ce "
