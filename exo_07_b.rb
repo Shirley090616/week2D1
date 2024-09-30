efa73e23f69777fd2c08abb85031f32c9ad7da6b
shirley Valeton <shirley.valeton88@gmail.com>
2024-09-30 22:15:35 +0200

    ajout des commentaires

--- exo_07_b.rb
+++ exo_07_b.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, c'est quoi ton blase ?"
-puts "> "
+print "> " #si c'est print la reponse de l'utlisateur reste à la ligne du signe > par contre sir je mets puts la repopnse de l'utilisateur se met en dessous du signe >
 user_name = gets.chomp
 puts user_name
