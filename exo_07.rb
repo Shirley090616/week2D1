12900f9e02afe149ed18897397483d35b2e3d573
shirley Valeton <shirley.valeton88@gmail.com>
2024-09-30 22:06:27 +0200

    rajout des commentaires

--- exo_07.rb
+++ exo_07.rb
@@ -1,3 +1,3 @@
 puts "Bonjour, c'est quoi ton blase ?"
-user_name = gets.chomp
-puts user_name
+user_name = gets.chomp #espace pour marque la reponse par l'utilisateur
+puts user_name #reponse repete
