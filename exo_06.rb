5e2270f36e2a0081ec8e5e756c4e36f23783fcdb
shirley Valeton <shirley.valeton88@gmail.com>
2024-09-30 21:53:01 +0200

    ajout du commentaire ligne 4 + ajout de la ligne 6

--- exo_06.rb
+++ exo_06.rb
@@ -1,4 +1,6 @@
 number_of_hours_worked_per_day = 10
 number_of_days_worked_per_week = 5
 number_of_weeks_in_THP = 11
-puts "travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}" #ça marque le resultat de l'operation
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
