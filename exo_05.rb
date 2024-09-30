2bf4572a53b6bd7d1de110ba2fc9d66114edd795
shirley Valeton <shirley.valeton88@gmail.com>
2024-09-30 21:41:16 +0200

    ? enlever à la ligne 7 + ajout des commentaires

--- exo_05.rb
+++ exo_05.rb
@@ -1,21 +1,21 @@
-puts "On va compter le nombre d'heures de travail à THP"
-puts "Travail : #{10 * 5 * 11}"
-puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+puts "On va compter le nombre d'heures de travail à THP" #ça marque le texte
+puts "Travail : #{10 * 5 * 11}" #ça marque Travail:le resultat
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #ça marque En minutes ça fait: le resultat
 
-puts "Et en secondes ?"
+puts "Et en secondes ?" #ça remarque la question
 
-puts 10 * 5 * 11 * 60 * 60
+puts 10 * 5 * 11 * 60 * 60 #Le resultat
 
-puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7" #ça remarque la même chose
 
-puts 3 + 2 < 5 - 7
-puts "ça fait combien 3 + 2 ? #{3 + 2}"
-puts "ça fait combien 5 - 7 ? #{5 - 7}"
+puts 3 + 2 < 5 - 7 # ça marque false
+puts "ça fait combien 3 + 2 ? #{3 + 2}" #ça marque 3 + 2 ? 5
+puts "ça fait combien 5 - 7 ? #{5 - 7}" #ça marque 5 -7 ? -2
 
-puts "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !"#ça marque la même chose
 
-puts "c'est drole ça, faisons-en plus :"
+puts "c'est drole ça, faisons-en plus :"#ça marque la même chose
 
-puts "Est-ce que 5 est plus grand que -2? #{5 > -2} "
-puts "Est-ce que 5 est superieur ou egal à -2? #{5 >= -2}"
-puts "Est-ce que 5 est inférieur ou egal à -2? #{5 >= -2}"
+puts "Est-ce que 5 est plus grand que -2? #{5 > -2} "#ça marque la même chose + la reponse qui est true
+puts "Est-ce que 5 est superieur ou egal à -2? #{5 >= -2}"#ça marque la même chose + la reponse qui est true
+puts "Est-ce que 5 est inférieur ou egal à -2? #{5 >= -2}"#ça marque la même chose + la reponse qui est true
