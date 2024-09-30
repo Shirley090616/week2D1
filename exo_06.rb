258979c56462fd0b8b381d8b8d6219218b0c7c07
shirley Valeton <shirley.valeton88@gmail.com>
2024-09-30 21:58:33 +0200

    copie de la ligne 6 sur la ligne 7 avec message en anglai

--- exo_06.rb
+++ exo_06.rb
@@ -4,3 +4,4 @@ number_of_weeks_in_THP = 11
 puts "travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}" #ça marque le resultat de l'operation
 
 puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "And in minutes that's : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
