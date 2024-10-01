896bec921a5f3c1b4ddc6b66dd4594d8e0e51aab
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-02 00:40:55 +0200

    creation fichier

---
+++ loops_01.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Ok, c'est cool, mais testons avec une nouvelle méthode."
+5.times do
+  puts "Bonjour, monde !"
+end
+# ça affiche 5 bonjour + les 5 fois bonjour
