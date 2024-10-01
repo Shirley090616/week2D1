b8e45613a26a79de6e828cf5024ce795b2d890ed
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-02 00:51:34 +0200

    creation fichier

---
+++ loops_05.rb
@@ -0,0 +1,7 @@
+5.times do |i|
+    puts "=== Boucle n°#{i} ==="
+    i.times do |j|
+      puts j
+    end 
+  end
+  # boucle n°0, rien, boucle n°1 il y a 0, boucle 2 il y a 0 et 1, boucle 3 il y a de 0 à 2, boucle 4 il y a de 0 à 3
