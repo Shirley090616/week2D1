0850f3c04b635ff8fdd594e840e479bb2e77e2ec
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 09:36:48 +0200

    ajout des commentaires

--- exo_11.rb
+++ exo_11.rb
@@ -1,6 +1,6 @@
-puts "écris un nombre"
-number = gets.chomp.to_i
+puts "écris un nombre" # reecris la phrase
+number = gets.chomp.to_i #l'utilisateur doit ecrire un nombre
 
-number.times do
+number.times do # ecris le "salut, ça farte" le nombre de fois correspondant au nombre qu'à mis l'utilisateur
     puts "Salut, ça farte"
   end
