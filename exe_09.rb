1d6011833ab03ab44c2cab306f61c9230853c37e
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 10:06:01 +0200

    ajout des commentaires

--- exe_09.rb
+++ exe_09.rb
@@ -1,7 +1,7 @@
-print "Quel est ton prénom ? "
-prenom = gets.chomp
+print "Quel est ton prénom ? " # remets le texte
+prenom = gets.chomp # l'utilisateur mets son prenom
 
-print "Quel est ton nom de famille ? "
-nom = gets.chomp
+print "Quel est ton nom de famille ? " #remets le texte
+nom = gets.chomp #l'utilisateur mets son nom
 
-puts "Bonjour, #{prenom} #{nom} !"
+puts "Bonjour, #{prenom} #{nom} !" # ça mets Bonjour, prenom et nom que l'utilisateur à mis
