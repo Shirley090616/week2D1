d3017bd4ce383d830455193513cbffe2f6747f7e
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 09:30:48 +0200

    ajout des commentaires

--- exo_10.rb
+++ exo_10.rb
@@ -1,4 +1,4 @@
-puts "Bonjour, quelle est ton année de naissance ?"
-user_birth_year = gets.chomp.to_i
+puts "Bonjour, quelle est ton année de naissance ?" #remarque la question
+user_birth_year = gets.chomp.to_i #demande l'année de naissance de l'utilisateur
 
-puts "tu avais #{2017 - user_birth_year} ans en 2017 "
+puts "tu avais #{2017 - user_birth_year} ans en 2017 " # reponse "tu avais 29 ans en 2017"
